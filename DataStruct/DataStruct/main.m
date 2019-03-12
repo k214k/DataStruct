79d21e748cbb1f557abe13b321cd563801213186
k214k <k214k@126.com>
2019-03-12 14:56:38 +0800

    20190312

---
+++ DataStruct/DataStruct/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DataStruct
+//
+//  Created by zhangliang on 2019/3/12.
+//  Copyright © 2019年 zl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
